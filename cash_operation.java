3656d711da934bb159801eccb8864fa5ddec01c8
ZHANG, Lu <57679092+LuZHANG1109@users.noreply.github.com>
2022-08-18 17:55:00 +0800

    Update cash_operation.java

--- cash_operation.java
+++ cash_operation.java
@@ -1,6 +1,7 @@
 package cash_operations;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 import cash_operations.Utility;
@@ -19,8 +20,9 @@ public class cash_operation {
 			System.out.println("2. Money deposit");
 			System.out.println("3. Money withdrawal");
 			System.out.println("4. Money transfer");
-			System.out.println("5. Exit\n");
-			System.out.println("Choose(0-5):");
+			System.out.println("5. Display transaction statement");
+			System.out.println("6. Exit\n");
+			System.out.println("Choose(0-6):");
 			char selection=Utility.readMenuSelection();
 			switch(selection) {
 			case'0':
@@ -47,6 +49,9 @@ public class cash_operation {
 			case'2':
 				System.out.println("Username: ");
 				String d_username=Utility.readString();
+				System.out.print("Currency:");
+				String d_cur=Utility.readString();
+				System.out.print("Withdrawal Amount: ");
 				System.out.print("Deposit Amount: ");
 				int money_in=Utility.readNumber();
 				System.out.print("Deposit Currency: ");
@@ -60,6 +65,15 @@ public class cash_operation {
 					for(int i=0;i<accounts.size();i++) {
 						if(accounts.get(i)[0].equals(d_username)) {
 							accounts.get(i)[2]=Integer.toString(Integer.parseInt(accounts.get(i)[2])+money_in);
+							String[] txinfo=new String[5];
+							Date time=new Date(); 
+							SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss"); 
+							txinfo[0]=df.format(time);
+							txinfo[1]=d_cur;
+							txinfo[2]="Deposit";
+							txinfo[3]="+"+Integer.toString(money_in);
+							txinfo[4]=d_username;
+							transactions.add(txinfo);
 						}
 					}
 					System.out.println("-------Deposit succeed---------");
@@ -69,6 +83,8 @@ public class cash_operation {
 			case'3':
 				System.out.println("Username: ");
 				String w_username=Utility.readString();
+				System.out.print("Currency:");
+				String w_cur=Utility.readString();
 				System.out.print("Withdrawal Amount: ");
 				int money_out=Utility.readNumber();
 				System.out.print("Withdrawal Description");
@@ -79,6 +95,15 @@ public class cash_operation {
 							System.out.print("Overdrew!");
 						}else {
 							accounts.get(i)[2]=Integer.toString(Integer.parseInt(accounts.get(i)[2])-money_out);
+							String[] txinfo=new String[5];
+							Date time=new Date(); 
+							SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss"); 
+							txinfo[0]=df.format(time);
+							txinfo[1]=w_cur;
+							txinfo[2]="Withdrawal";
+							txinfo[3]="-"+Integer.toString(money_out);
+							txinfo[4]=w_username;
+							transactions.add(txinfo);
 							System.out.println("-------Withdrawal succeed---------");
 						}
 					}
@@ -96,7 +121,7 @@ public class cash_operation {
 				OSL_FEE+=0.01*_amount;
 				_amount-=OSL_FEE;
 				for(int i=0;i<accounts.size();i++) {
-					if(accounts.get(i).equals(fromname)) {
+					if(accounts.get(i)[0].equals(fromname)) {
 						if(Integer.parseInt(accounts.get(i)[2])<_amount){
 							System.out.print("Transfer Failed:");
 						}else {
@@ -105,17 +130,35 @@ public class cash_operation {
 					}
 				}
 				for(int i=0;i<accounts.size();i++) {
-					if(accounts.get(i).equals(toname)) {
+					if(accounts.get(i)[0].equals(toname)) {
 						accounts.get(i)[2]=Integer.toString(Integer.parseInt(accounts.get(i)[2])+_amount);
 					}
 				}
-				String[] txinfo=new String[4];
+				String[] in_txinfo=new String[5];
 				Date time=new Date(); 
 				SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss"); 
-				txinfo[0]=df.format(time);
-				txinfo[1]=_cur;
-				txinfo[3]=Integer.toString(_amount);
+				in_txinfo[0]=df.format(time);
+				in_txinfo[1]=_cur;
+				in_txinfo[2]="Transfer In";
+				in_txinfo[3]="+"+Integer.toString(_amount);
+				in_txinfo[4]=toname;
+				transactions.add(in_txinfo);
+				String[] out_txinfo=new String[5];
+				out_txinfo[0]=df.format(time);
+				out_txinfo[1]=_cur;
+				out_txinfo[2]="Transfer Out";
+				out_txinfo[3]="-"+Integer.toString(_amount);
+				out_txinfo[4]=fromname;
+				transactions.add(out_txinfo);
 			case'5':
+				System.out.println("Username: ");
+				String tx_username=Utility.readString();
+				for(int i=0;i<transactions.size();i++) {
+					if(transactions.get(i)[0].equals(tx_username)) {
+						System.out.println(Arrays.toString(transactions.get(i)));
+					}
+				}
+			case'6':
 				System.out.print("Confirm(Y/N):");
 				char isExist=Utility.readConfirmSelection();
 				if(isExist=='Y') {
